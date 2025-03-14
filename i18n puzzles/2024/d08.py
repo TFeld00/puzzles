6a6f36312262961375c62b9d730993d9f1672e01
TFeld00 <tfeld00@gmail.com>
2025-03-14 13:11:38 +0100

    .

--- i18n puzzles/2024/d08.py
+++ i18n puzzles/2024/d08.py
@@ -6,11 +6,11 @@ from unidecode import unidecode
 s=0
 
 def valid(s):
+    s=unidecode(s).lower()
     if len(s)<4:return False
     if len(s)>12:return False
-    s=unidecode(s).lower()
-    if not any(c.lower() in 'aeiou' for c in s):return False
-    if not any(c.isalpha() and (c.lower() not in 'aeiou')for c in s):return False
+    if not any(c in 'aeiou' for c in s):return False
+    if not any(c.isalpha() and (c not in 'aeiou')for c in s):return False
     if not any(c.isdigit()for c in s):return False
     if any(a.isalpha() and b>1 for a,b in Counter(s).items()):return False
     return True
