a61eba4eb9784e0ba5610cc2696120ca3cfaf91f
TFeld00 <tfeld00@gmail.com>
2025-03-18 13:56:43 +0100

    prettify

--- i18n puzzles/2024/d12.py
+++ i18n puzzles/2024/d12.py
@@ -1,93 +1,44 @@
 DAY,_,_=__file__.rpartition('.')
 
-import os
-from itertools import *
-from collections import *
-_pow = pow
-from math import *
-pow = _pow
-from string import ascii_lowercase, ascii_uppercase, ascii_letters, digits, hexdigits, whitespace, punctuation, printable
-from alg.dijkstra import dijkstra
-from img.img import read_img, write_img, write_img_fromlist     #write_img(DAY,COLS)
-from alg.util import parse_with_headers, parse_skip_headers, parse_no_headers
-from alg.util import get_neigbors_both, get_neigbors_diag, get_neigbors_orto, get_neighbor_positions, get_neighbor_positions_complex
-from alg.util import get_bounds, get_bounds_complex, get_bounds_complex_dict
-from alg.util import rotate90clockwise, rotate180, rotate90counterclockwise
-from alg.grid import word_search, pattern_search
-from alg.floodfill import fill, global_fill_dist, global_fill_dist_diags
-from alg.cellular import step_dict, step_list, to_dict, to_lists, step_function_game_of_life
-from alg.string import shift_caesar, tr, block_print, readable_number, findall_overlapping
-from alg.geom import area, area_pixels
-from functools import *
-from fractions import *
-from textwrap import wrap
-from datetime import *
-from queue import *
-import heapq #heapq.nsmallest
-from functools import cache
-import sympy    #sympy.primefactors, sympy.solve, sympy.sympify, etc..
 import re
-import sys
-import z3
-import networkx as nx
-import locale
-import pytz
 from unidecode import *
 
-COLS = {
-    '.': (255, 255, 255),
-    '#': (0, 0, 0),
-}
-
 r=[]
-s=0
-t=''
 
 with open(f'{DAY}.txt', 'r', encoding='utf8')as F:
     for l in F:
         l=l.rstrip('\n')
-        #l=[*map(int,re.findall('-?\d+',l))]
-        #l=[*map(int,l.split())]
-        #l=[*map(int,l)]
-        #l=l.split()
-        #l=int(l)
-        #l=list(l)
-        #print(l)
         r+=[l]
-        # t+=l
-        
-        
-#W,H=len(r[0]),len(r)
+
+def sortGen(s,pre,post, lower=True):
+    s=re.sub('\d|[^,\w]','',s)
+    if lower:s=s.lower()
+    s=pre(s)
+    s=unidecode(s)
+    s=post(s)
+    return s.split(',')
 
 def sortE(s):
-    s=re.sub('\d|[^,\w]','',s)
-    s=s.lower()
-    s=s.replace('æ','ae')
-    s=unidecode(s)
-    s=s.split(',')
-    return s
+    return sortGen(
+        s,
+        lambda s:s.replace('æ','ae'),
+        lambda s:s
+    )
 
 def sortS(s):
-    s=re.sub('\d|[^,\w]','',s)
-    s=s.lower()
-    t=str.maketrans('åäöæø','ABCBC')
-    s=str.translate(s,t)
-    s=unidecode(s)
-    t=str.maketrans('ABC','åäö')
-    s=str.translate(s,t)
-    s=s.split(',')
-    return s
+    return sortGen(
+        s,
+        lambda s:str.translate(s,str.maketrans('åäöæø','ABCBC')),
+        lambda s:str.translate(s,str.maketrans('ABC','åäö'))
+    )
 
 def sortD(s):
-    s=re.sub('\d|[^,\w]','',s)
-    s=s.replace('æ','ae')
-    s=unidecode(s)
-    a,b=re.search('([a-z ]*)([A-Z].*)',s).groups()
-    s=b.replace(':',a+':')
-    s=s.lower()
-    s=s.split(',')
-    return s
-
+    return sortGen(
+        s,
+        lambda s:s.replace('æ','ae'),
+        lambda s:''.join(re.search('([^A-Z]*)([A-Z].*)',s).groups()[::-1]).lower(),
+        lower=False
+    )
 
 le=sorted(r,key=sortE)
 ls=sorted(r,key=sortS)
