eaf2afb1982f4f14be2e01742f0a0d46cb0ad44e
TFeld00 <tfeld00@gmail.com>
2025-03-20 13:53:45 +0100

    i18n - day 14 - actual parsing of numbers

--- i18n puzzles/2024/d14.py
+++ i18n puzzles/2024/d14.py
@@ -1,6 +1,7 @@
 DAY,_,_=__file__.rpartition('.')
 
 from fractions import *
+import re
 import japanese_numbers
 
 r=[]
@@ -30,6 +31,42 @@ s=0
 
 for l in r:
     a,b = japanese_numbers.to_arabic_numbers(l)
+    ua,ub = getUnits(l)
+    x=(a*ua*b*ub)
+    s+=x
+
+print(s)
+
+# manual parsing
+N='一二三四五六七八九十'
+T={
+    '百':100,
+    '千':1000,
+    '万':10000,
+    '億':100000000,
+}
+
+def parse(s):
+    d=dict(T)
+    for i,c in enumerate(N,1):
+        d[c]=i
+    p=0
+    for a,b in re.findall('([一二三四五六七八九]?)([十百千]?)',s):
+        if a or b:
+            p+=d.get(a,1)*d.get(b,1)
+    return p
+
+def getNumbers(s):
+    c=N+''.join(T.keys())
+    for n in re.findall(f'[{c}]+',s):
+        t=0
+        for a,b in re.findall(r'([^万億]*)(万|億)?',n):
+            t+=parse(a)*T.get(b,1)
+        yield t
+
+s=0
+for l in r:
+    a,b = getNumbers(l)
     ua,ub = getUnits(l)
     x=(a*ua*b*ub)
     s+=x
