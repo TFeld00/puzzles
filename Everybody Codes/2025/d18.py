b2ced5ae300dbc57f6280edc826c78270dcb23d2
TFeld00 <tfeld00@gmail.com>
2025-11-27 00:28:14 +0100

    Everybody Codes - 2025 - Day 18

---
+++ Everybody Codes/2025/d18.py
@@ -0,0 +1,161 @@
+DAY,_,_=__file__.rpartition('.')
+
+from alg.util import parse_no_headers
+import re
+
+r=[]
+s=0
+t=''
+
+with open(f'{DAY}a.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        l=[*map(int,re.findall(r'-?\d+',l))]
+        r+=[l]
+        
+r = parse_no_headers(r)
+
+F={}
+P={}
+E={}
+last=0
+for p,*b in r:
+    v,w=p
+    P[v]=w
+    for q in b:
+        if len(q)==1:
+            F[v]=q[0]
+        else:
+            x,y=q
+            if v not in E:
+                E[v]=[]
+            E[v]+=[x,y],
+    last = v
+
+def f(n):
+    s=0
+    if n in F:
+        return F[n]
+    for v,w in E[n]:
+        s+=f(v)*w
+    if s>=P[n]:
+        return s
+    return 0
+
+print(f(last))
+
+
+
+# ---
+
+
+r=[]
+s=0
+t=''
+
+with open(f'{DAY}b.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        l=[*map(int,re.findall(r'-?\d+',l))]
+        r+=[l]
+        
+*r,_,TEST = parse_no_headers(r)
+
+F={}
+P={}
+E={}
+last=0
+for p,*b in r:
+    v,w=p
+    P[v]=w
+    for q in b:
+        if len(q)==1:
+            F[v]=q[0]
+        else:
+            x,y=q
+            if v not in E:
+                E[v]=[]
+            E[v]+=[x,y],
+    last = v
+
+def f(n):
+    s=0
+    if n in F:
+        return F[n]
+    for v,w in E[n]:
+        s+=f(v)*w
+    if s>=P[n]:
+        return s
+    return 0
+
+S=0
+for t in TEST:
+    for i,v in enumerate(t,1):
+        F[i]=v
+    S+=f(last)
+print(S)
+
+
+
+# - 
+
+
+r=[]
+s=0
+t=''
+
+with open(f'{DAY}c.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        l=[*map(int,re.findall(r'-?\d+',l))]
+        r+=[l]
+        
+*r,_,TEST = parse_no_headers(r)
+
+F={}
+P={}
+E={}
+ALL={}
+last=0
+for p,*b in r:
+    v,w=p
+    P[v]=w
+    for q in b:
+        if len(q)==1:
+            F[v]=q[0]
+        else:
+            x,y=q
+            if v not in E:
+                E[v]=[]
+            E[v]+=[x,y],
+            if x not in ALL:
+                ALL[x]=[]
+            ALL[x]+=y,
+    last = v
+
+def f(n):
+    s=0
+    if n in F:
+        return F[n]
+    for v,w in E[n]:
+        s+=f(v)*w
+    if s>=P[n]:
+        return s
+    return 0
+
+for v,w in ALL.items():
+    if v in F:
+        if sum(w)<=0:
+            F[v]=0
+        else:
+            F[v]=1
+
+MAX=f(last)
+S=0
+for t in TEST:
+    for i,v in enumerate(t,1):
+        F[i]=v
+    x=f(last)
+    if x:
+        S+=MAX-x
+print(S)
