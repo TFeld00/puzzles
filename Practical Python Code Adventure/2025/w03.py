597717ff154081b6da399b7c24b40292455456c5
TFeld00 <tfeld00@gmail.com>
2026-07-06 09:41:16 +0200

    ppca 2025 - w03

--- Practical Python Code Adventure/2025/w03.py
+++ Practical Python Code Adventure/2025/w03.py
@@ -4,8 +4,26 @@ r=[]
 with open(f'{DAY}.1.txt','r', encoding='utf8')as F:
     for l in F:
         l=l.rstrip('\n')
-        r+=l,
+        r+=l.replace('_',' ').split(',') if l else [],
 
+p,i,*r=r
+p=int(p[0])
+t=[]
+
+for x in i:
+    o,n=x[0],int(x[1:])
+    if o=='L':
+        p-=n
+    elif o=='R':
+        p+=n
+    elif o=='T':
+        t+=r[p][:n]
+        r[p]=r[p][n:]
+    elif o=='D':
+        r[p]+=t[:n]
+        t=t[n:]
+
+print(''.join([v[0]for v in r if v]))
 
 # ---
 
@@ -13,4 +31,24 @@ r=[]
 with open(f'{DAY}.2.txt','r', encoding='utf8')as F:
     for l in F:
         l=l.rstrip('\n')
-        r+=l,
+        r+=l.replace('_',' ').split(',') if l else [],
+
+p,i,*r=r
+p=int(p[0])
+t=[]
+
+for x in i:
+    o,n=x[0],int(x[1:])
+    if o=='L':
+        p-=n
+    elif o=='R':
+        p+=n
+    elif o=='T':
+        t+=r[p][:n]
+        r[p]=r[p][n:]
+        r=r[1:]+r[:1]
+    elif o=='D':
+        r[p]+=t[:n]
+        t=t[n:]
+
+print(''.join([v[0]for v in r if v]))
