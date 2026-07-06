b4a44d3f8d914be57dd5be7acaa5d57c4b29d1db
TFeld00 <tfeld00@gmail.com>
2026-07-06 13:29:00 +0200

    ppca 2025 w7+8

--- Practical Python Code Adventure/2025/w07.py
+++ Practical Python Code Adventure/2025/w07.py
@@ -1,16 +1,36 @@
 DAY,_,_=__file__.rpartition('.')
 
-r=[]
+from collections import Counter
+from PIL import Image
+
+
+s=''
 with open(f'{DAY}.1.txt','r', encoding='utf8')as F:
     for l in F:
         l=l.rstrip('\n')
-        r+=l,
+        s+=l
 
+C=Counter(s[i:i+4]for i in range(0,len(s),4))
+D={v:x for x,v in C.items()}
+
+print(D[1081],D[1055],D[965],sep='')
 
 # ---
 
 r=[]
-with open(f'{DAY}.2.txt','r', encoding='utf8')as F:
-    for l in F:
-        l=l.rstrip('\n')
-        r+=l,
+with Image.open(f"{DAY}.2.png") as img:
+    pixels = img.load()
+    w, h = img.size
+    for i in range(h):
+        l = []
+        for j in range(w):
+            l += pixels[j, i],
+        r += l
+
+C=Counter(r)
+x=[]
+t=str.maketrans('0123456789','OIZEASGTBP')
+for v,_ in C.most_common(3):
+    s='%02X%02X%02X'%v
+    x+=s.translate(t),
+print(*x)
--- Practical Python Code Adventure/2025/w08.py
+++ Practical Python Code Adventure/2025/w08.py
@@ -1,16 +1,50 @@
 DAY,_,_=__file__.rpartition('.')
 
+from functools import cache
+
 r=[]
 with open(f'{DAY}.1.txt','r', encoding='utf8')as F:
     for l in F:
         l=l.rstrip('\n')
         r+=l,
+R=[]
+for l in r[1:]:
+    c,b,o,w,x=l.split(',')
+    R+=(c,b,o,int(w),int(x)),
 
+
+@cache
+def f1(i,w):
+    if w<0:return -1e9
+    if w==0 or not R[i:]:
+        return 0
+    *_,v,x=R[i]
+    a=f1(i+1,w)
+    if w>=v:
+        return max(a, f1(i+1,w-v)+x)
+    return a
+    
+print(f1(0,300))
 
 # ---
 
-r=[]
-with open(f'{DAY}.2.txt','r', encoding='utf8')as F:
-    for l in F:
-        l=l.rstrip('\n')
-        r+=l,
+roasts = {v:1<<i for i,v in enumerate({v for _,_,v,_,_ in R})}
+beans = {v:1<<i for i,v in enumerate({v for _,v,_,_,_ in R})}
+all_roasts = (2**len(roasts))-1
+
+@cache
+def f2(i,b_used,r_used,w):
+    if w<0:return -1e9
+    if w==0 or not R[i:]:
+        if r_used==all_roasts:
+            return 0
+        return -1e9
+    _,b,r,v,x=R[i]
+    b=beans[b]
+    r=roasts[r]
+    a=f2(i+1,b_used,r_used,w)
+    if w>=v and b_used&b==0:
+        return max(a, f2(i+1,b_used | b,r_used | r,w-v)+x)
+    return a
+    
+print(f2(0,0,0,300))
